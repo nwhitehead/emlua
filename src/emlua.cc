39ada9756f730c4fa9cf947192ec1a779ddbc7a7
Nathan Whitehead <nwhitehe@gmail.com>
2016-04-21 21:28:47 -0700

    Organize into C library.

---
+++ src/emlua.cc
@@ -0,0 +1,37 @@
+
+#include "emlua.hh"
+
+#include <string>
+
+#include "selene.h"
+
+// Global state
+sel::State *g_state = nullptr;
+
+void init(void) {
+    if (g_state) {
+        deinit();
+    }
+    g_state = new sel::State{true};
+}
+
+void exec(const char *txt) {
+    if (g_state) {
+        (*g_state)(txt);
+    }
+}
+
+void deinit(void) {
+    if (g_state) {
+        delete g_state;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    init();
+    exec("print(42)");
+    deinit();
+
+    return 0;
+}
---
+++ src/emlua.hh
@@ -0,0 +1,12 @@
+
+#include <string>
+
+#include "selene.h"
+
+extern "C" {
+
+void init(void);
+void exec(const char *txt);
+void deinit(void);
+
+}

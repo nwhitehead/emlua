2c9e3f40d2713672775324a453733d511cf92142
Nathan Whitehead <nwhitehe@gmail.com>
2016-04-24 00:31:10 -0700

    NPM run script for building, working ace.

--- editor/src/main.js
+++ editor/src/main.js
@@ -1,11 +1,49 @@
 
-var emlua = EMLUA;
+/// ACE editor with styles and syntax highlighting
+var ace = require('ace');
+require('../vendor/ace/theme-twilight.js');
+require('../vendor/ace/mode-lua.js');
 
-var ace = require('../vendor/ace/ace-src-min-noconflict.js');
+/// Terminal
 var Console = require('./OutputConsole');
+require('../vendor/xterm.js/xterm.css');
 
+/// Main
 require('./main.css');
-require('vendor/xterm.js/xterm.css');
+var emlua = EMLUA;
+var terminal = Console.create('terminal-container');
+
+////////////////////////////////
+
+// Show banner
+var ansi_normal = "\x1b[0m";
+var ansi_purple = "\x1b[35m";
+var ansi_bold = "\x1b[1m";
+var ansi_yellow = "\x1b[33m";
+var msg = 
+    ansi_purple + ansi_bold + "ManiacWebSDK Output Console\n" + ansi_normal +
+    ansi_purple + "~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + ansi_normal +
+    ansi_purple + "Copyright 2016 Nathan Whitehead and others\n\n" + ansi_normal;
+
+terminal.write(msg);
+
+// Set up editor
+
+var editor =  ace.edit("editor");
+editor.setTheme("ace/theme/twilight");
+editor.getSession().setMode("ace/mode/lua");
+editor.setShowPrintMargin(false);
+editor.setFontSize(14);
+editor.$blockScrolling = Infinity;
+
+var txt_default = "\n-- Put your Lua code here\n\n";
+editor.setValue(txt_default);
+
+
+
+
+
+
 
 var l = emlua();
 

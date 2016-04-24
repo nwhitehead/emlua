8cac1c87fa3edc7f159f2848ce89d7fb9fb06a34
Nathan Whitehead <nwhitehe@gmail.com>
2016-04-24 11:38:18 -0700

    Revamp interface to control C space better.

--- src/emlua_interface.js
+++ src/emlua_interface.js
@@ -1,24 +1,18 @@
 
 // Emscripten module is always named EMLUA_C in global namespace.
 // Had trouble using require on it.
-var emlua_c = EMLUA_C({});
+var emlua_c = EMLUA_C;
 
-var init = emlua_c.cwrap('init', 'number', null);
-var exec = emlua_c.cwrap('exec', 'boolean', ['number', 'string', 'string', 'number']);
-var deinit = emlua_c.cwrap('deinit', null, ['number']);
-
-/// Create a new state (optionally use new keyword)
-var state = function() {
+/// State class, constructed from existing C state
+var state = function(parent) {
     // Check if called without new
     if (!(this instanceof state)) {
-        return new state();
+        return new state(parent);
     }
-    // Keep pointer to lua_State
-    this._L = init();
-}
-
-/// Get reference to C space
-state.prototype.module = emlua_c;
+    this._parent = parent;
+    this._L = parent._init();
+    return this;
+};
 
 /// Execute a string
 state.prototype.exec = function(txt, tag, show_traceback) {
@@ -28,32 +22,87 @@ state.prototype.exec = function(txt, tag, show_traceback) {
     if (!this._L) {
         throw "State has been destroyed with deinit()";
     }
-    var res = exec(this._L, txt, tag, show_traceback);
+    var res = this._parent._exec(this._L, txt, tag, show_traceback);
     return (res === 1);
-};
-
-/// Free up state
-state.prototype.deinit = function() {
-    if (this._L) {
-        deinit(this._L);
-        this._L = undefined;
-    } else {
-        throw "State has been destroyed with deinit()";
-    }
 };
 
 /// Reset state to fresh instance
 state.prototype.reset = function() {
     if (this._L) {
-        deinit(this._L);
+        this._parent._deinit(this._L);
         this._L = undefined;
     }
-    this._L = init();
+    this._L = this._parent._init();
+    return this;
+};
+
+/// Deinit
+state.prototype.deinit = function() {
+    if (this._L) {
+        this._parent._deinit(this._L);
+        this._L = undefined;
+        this._parent = undefined;
+    } else {
+        throw "State has been destroyed with deinit()";
+    }
+    return this;
+};
+
+/// Create a new main object (optionally use new keyword)
+var main = function(options) {
+    // Check if called without new
+    if (!(this instanceof main)) {
+        return new main();
+    }
+    // Keep track of options
+    var emlua_c = EMLUA_C(options);
+    this._emlua_c = emlua_c;
+    this._init = emlua_c.cwrap('init', 'number', null);
+    this._exec = emlua_c.cwrap('exec', 'boolean', ['number', 'string', 'string', 'number']);
+    this._deinit = emlua_c.cwrap('deinit', null, ['number']);
+    // Keep pointer to default state
+    this._state = state(this);
+}
+
+/// Create a new state in same C area
+main.prototype.state = function() {
+    if (this._emlua_c) {
+        return state(this);
+    } else {
+        throw "Main state has been destroyed with deinit()";
+    }
+};
+
+/// Execute a string
+main.prototype.exec = function(txt, tag, show_traceback) {
+    return this._state.exec(txt, tag, show_traceback);
+};
+
+/// Free up state
+main.prototype.deinit = function() {
+    if (this._state) {
+        this._state.deinit();
+        this._state = undefined;
+    }
+    if (this._emlua_c) {
+        this._init = undefined;
+        this._exec = undefined;
+        this._deinit = undefined;
+        this._emlua_c = undefined;
+    } else {
+        throw "Main state has been destroyed with deinit()";
+    }
+    return this;
+};
+
+/// Reset default state to fresh instance
+main.prototype.reset = function() {
+    return this._state.reset();
 };
 
 /// Exports
 if (typeof module === 'object' && module.exports) { /// Node
-    module.exports = state;
+    module.exports = main;
 } else {
-    EMLUA = state;
+    EMLUA = main;
 }
